67d11c746586f9e66ac230606a4dcde89a49ab50
rurtubiac <randy.urtubia@inacapmail.cl>
2015-05-30 22:00:31 -0300

    Finished project

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,12 +5,36 @@ import time
 # variables:
 tries = 0
 name = ''
-number = random.randint(0,100)
+number = random.randint(0,101)
+found = False
 
+# intro
 print("in this game you will have to guess a number in the least number of tries.")
 time.sleep(1)
 print("First, I would like to know your name.")
 time.sleep(.5)
 print("What's yor name?")
 name = input()
+print("Thank you", name ,".I have selected a number between 0 and 100, try to guess it!")
+
+# game loop
+while found == False:
+    tries +=1
+    print("What is your guess number ",tries,"?")
+    guess = input()
+
+    #casting the input as an int
+    guess = int(guess)
+
+    if guess < number:
+        print("Your number is lower than mine, try again")
+
+    if guess > number:
+        print("Your number is higher than mine, try again")
+
+    if guess == number:
+        found = True
+        break
+
+print("Congratulations! You found my number in",tries,"tries")
 
