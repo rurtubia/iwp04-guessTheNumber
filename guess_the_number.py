15e757f8ca622d99b820a3659830ed5835f39805
rurtubiac <randy.urtubia@inacapmail.cl>
2015-05-30 21:33:42 -0300

    updated imports

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,16 @@
 # import random number generator
 import random
+import time
 
 # variables:
 tries = 0
+name = ''
+number = random.randint(0,100)
+
+print("in this game you will have to guess a number in the least number of tries.")
+time.sleep(1)
+print("First, I would like to know your name.")
+time.sleep(.5)
+print("What's yor name?")
+name = input()
 
